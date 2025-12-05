663bf16b53e0989b3ec5e5594855cdb1c26bebc2
tebatsomaapola <58773321+tebatsomaapola@users.noreply.github.com>
2025-12-05 15:49:01 +0200

    Add files via upload

---
+++ Common/AuthRoles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WorkspaceTenants.Common{
+
+    public static class AuthRoles
+    {
+      public const string TenantAdmin = "Admin";
+      public const string Member = "Member";
+      public const string ReadOnly = "ReadOnly";
+    }
+}
---
+++ Common/Result.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WorkspaceTenants.Common{
+
+public record Result(bool Success, string Error = null)
+{
+    public static Result Ok() => new(true);
+    public static Result Fail(string error) => new(false, error);
+}
+}
