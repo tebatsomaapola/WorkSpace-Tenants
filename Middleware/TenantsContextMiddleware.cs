2f5461229cf084252e0482ab1b2d8e9e7b70186c
tebatsomaapola <58773321+tebatsomaapola@users.noreply.github.com>
2025-12-05 15:35:46 +0200

    Add files via upload

---
+++ Middleware/TenantsContextMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using WorkspaceTenants.Services;
+using System.Threading.Tasks;
+
+
+namespace WorkspaceTenants.Middleware{
+
+public class TenantContextMiddleware
+{
+    private readonly RequestDelegate _next;
+    public TenantContextMiddleware(RequestDelegate next) => _next = next;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.User?.Identity?.IsAuthenticated == true)
+        {
+            context.Items["TenantContext"] = new TenantContext(context.User);
+        }
+        await _next(context);
+    }
+}
+
+public static class TenantContextExtensions
+{
+    public static ITenantContext GetTenantContext(this HttpContext context)
+        => (ITenantContext)context.Items["TenantContext"]!;
+}
+}
